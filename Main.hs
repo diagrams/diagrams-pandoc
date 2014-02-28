40dfb19df9b182e4a965f5d2213bb679e0802f16
Daniel Bergey <bergey@alum.mit.edu>
2014-02-28 17:52:40 +0000

    initial draft: works in limited cases

---
+++ Main.hs
@@ -0,0 +1,103 @@
+import           System.FilePath                    (joinPath, splitPath, (<.>),
+                                                     (</>))
+import           System.IO
+import           Diagrams.Backend.Cairo
+import           Diagrams.Backend.Cairo.Internal
+import           Control.Monad                      (when)
+import qualified Diagrams.Builder                   as DB
+import           System.Directory                   (createDirectory,
+                                                     doesDirectoryExist)
+import           Data.VectorSpace                   (zeroV)
+import Text.Pandoc.JSON
+import           Diagrams.TwoD.Size                 (SizeSpec2D (Dims))
+import           Diagrams.Prelude                   (centerXY, pad, (&), (.~))
+import Control.Applicative
+import Data.List (delete)
+
+-- TODO choose output format based on pandoc target
+backendExt = "png"
+
+main :: IO ()
+main = toJSONFilter $ insertDiagrams "images"
+
+dropDiagrams (CodeBlock (_,["diagram"],_) code) = []
+dropDiagrams block = [block]
+
+insertDiagrams :: FilePath -> Block -> IO [Block]
+insertDiagrams outDir (CodeBlock (ident, classes, attrs) code)
+    | "diagram-haskell" `elem` classes = (++ [bl']) <$> img
+    | "diagram" `elem` classes = img
+  where
+    img = do
+        d <- compileDiagram outDir code
+        return $ case d of
+            Left _err     -> []
+            Right imgName -> [Plain [Image [] (imgName,"")]] -- no alt text, no title
+    bl' = CodeBlock (ident, "haskell":delete "diagram-haskell" classes, attrs) code
+insertDiagrams _ block = return [block]
+
+-- Copied from https://github.com/diagrams/diagrams-doc/blob/master/doc/Xml2Html.hs
+-- With the CPP removed, thereby requiring Cairo
+-- TODO clean this up, move it into -builder somehow
+-- | Compile the literate source code of a diagram to a .png file with
+--   a file name given by a hash of the source code contents
+compileDiagram :: FilePath -> String -> IO (Either String String)
+compileDiagram outDir src = do
+  ensureDir outDir
+
+  let bopts = DB.mkBuildOpts
+
+                Cairo
+
+                zeroV
+
+                (CairoOptions "default.png" (Dims 500 200) PNG False)
+
+                & DB.snippets .~ [src]
+                & DB.imports  .~
+                  [ "Diagrams.TwoD.Types"      -- WHY IS THIS NECESSARY =(
+                  , "Diagrams.Core.Points"
+                      -- GHC 7.2 bug?  need  V (Point R2) = R2  (see #65)
+                  , "Diagrams.Backend.Cairo"
+                  , "Diagrams.Backend.Cairo.Internal"
+                  , "Graphics.SVGFonts"
+                  , "Data.Typeable"
+                  ]
+                & DB.pragmas .~ ["DeriveDataTypeable"]
+                & DB.diaExpr .~ "example"
+                & DB.postProcess .~ (pad 1.1 . centerXY)
+                & DB.decideRegen .~
+                  (DB.hashedRegenerate
+                    (\hash opts -> opts { _cairoFileName = mkFile hash })
+                    outDir
+                  )
+
+  res <- DB.buildDiagram bopts
+
+  case res of
+    DB.ParseErr err    -> do
+      hPutStrLn stderr ("\nError while parsing\n" ++ src)
+      hPutStrLn stderr err
+      return $ Left "Error while parsing"
+
+    DB.InterpErr ierr  -> do
+      hPutStrLn stderr ("\nError while interpreting\n" ++ src)
+      hPutStrLn stderr (DB.ppInterpError ierr)
+      return $ Left "Error while interpreting"
+
+    DB.Skipped hash    -> do
+      hPutStr stderr "."
+      hFlush stderr
+      return $ Right (mkFile (DB.hashToHexStr hash))
+
+    DB.OK hash out -> do
+      hPutStr stderr "O"
+      hFlush stderr
+      fst out
+      return $ Right (mkFile (DB.hashToHexStr hash))
+
+ where
+  mkFile base = outDir </> base <.> backendExt
+  ensureDir dir = do
+    b <- doesDirectoryExist dir
+    when (not b) $ createDirectory dir
---
+++ README.md
@@ -0,0 +1,56 @@
+A [pandoc](http://johnmacfarlane.net/pandoc/) filter to express
+diagrams inline using the haskell EDSL
+[diagrams](http://projects.haskell.org/diagrams/).
+
+## Usage
+
+Create an input file called `demo.md` with the following text:
+
+``` markdown
+    This is how to draw a circle:
+    ~~~ diagram
+    example = circle 1
+    ~~~
+```
+
+Install diagrams-pandoc, then run pandoc as follows:
+
+``` bash
+    pandoc -t html demo.md --filter diagrams-pandoc -o demo.html -s
+```
+
+The file demo.html should now have an img tag pointing at a PNG of a circle.
+
+## Details
+
+`diagrams-pandoc` compiles code blocks containing diagrams expressions
+and includes the resulting images in the pandoc markup.  It is meant
+to be run as a
+[pandoc filter](http://johnmacfarlane.net/pandoc/scripting.html) as
+shown below.
+
+`diagrams-pandoc` is aware of two code block classes.  A block with
+the `diagram` class will be replaced by the resulting image---the code
+will not appear in the output.  A block with the `diagram-haskell`
+class will produce both an image and a (syntax highlighted) code
+block.  The input block is replaced by image appears before the code
+block, and the `diagram-haskell` class is replaced by the `haskell`
+class, so that pandoc can perform syntax highlighting as usual.
+
+## Installing
+
+`diagrams-pandoc` is not yet on Hackage.  To install, clone this
+repository, and install with cabal.
+
+``` bash
+    git clone https://github.com/bergey/diagrams-pandoc.git
+    cd diagrams-pandoc
+    cabal install
+```
+
+## TODO
+
+* use pandoc output type to pick an image file format
+* for formats which are more human-readable (eg, markdown, rst), leave
+  code block alone?
+* provide command-line flags to override default behavior
