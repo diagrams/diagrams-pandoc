f1f13f3148cf70ed713890a117d2b74caae95da8
Berk Ozkutuk <ozkutuk@protonmail.com>
2023-08-04 11:12:18 +0200

    Minor refactor and documentation

--- test/Main.hs
+++ test/Main.hs
@@ -1,33 +1,35 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
-import qualified Test.Tasty as Tasty
 import Test.Tasty (TestTree)
+import qualified Test.Tasty as Tasty
 import qualified Test.Tasty.Golden as Tasty
+
 import qualified Text.Pandoc as Pandoc
-import System.FilePath (replaceExtension, takeBaseName)
-import qualified Data.ByteString as B
-import Data.ByteString (ByteString)
-import Text.Pandoc.Diagrams (Opts(..), Backend (..), insertDiagrams)
 import Text.Pandoc.Options (def, readerExtensions, pandocExtensions, writerExtensions)
-import qualified Data.ByteString.Lazy as LBS
 import qualified Text.Pandoc.Walk as Pandoc
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
+
 import Control.Monad.IO.Class (liftIO)
-import Data.Text.Encoding (encodeUtf8)
+import qualified Data.ByteString.Lazy as LBS
+import Data.Text (Text)
+import qualified Data.Text.IO as T
+import qualified Data.Text.Encoding as T
 import System.Directory (removeDirectoryRecursive)
+import System.FilePath (replaceExtension, takeBaseName)
+
+import Text.Pandoc.Diagrams (Opts (..), Backend (..), insertDiagrams)
+
 
 main :: IO ()
-main = do
-  tests <- goldenTests
-  let tests' = Tasty.withResource (pure ()) (const cleanup) (const tests)
-  Tasty.defaultMain tests'
+main = Tasty.defaultMain . withCleanup =<< goldenTests
+
+-- Cleanup the generated "images" directory after the test runs complete
+withCleanup :: TestTree -> TestTree
+withCleanup tt = Tasty.withResource (pure ()) (const cleanup) (const tt)
   where
     cleanup :: IO ()
     cleanup = removeDirectoryRecursive (_outDir defaultOpts)
 
+-- Extracted from the CLI parser
 defaultOpts :: Opts
 defaultOpts = Opts
   { _outFormat = "html"
@@ -40,10 +42,13 @@ defaultOpts = Opts
 diagramsFilter :: Pandoc.Pandoc -> IO Pandoc.Pandoc
 diagramsFilter = Pandoc.walkM (fmap concat . mapM (insertDiagrams defaultOpts))
 
-mdToNative :: String -> IO T.Text
+mdToNative :: String -> IO Text
 mdToNative f = do
   mdContents <- T.readFile f
   Pandoc.runIOorExplode $ do
+    -- Pandoc extensions that are enabled by default in Pandoc CLI are also
+    -- enabled here, because the input files make use of some fancy syntax
+    -- (backtick_code_blocks, fenced_code_attributes, etc.)
     ast <- Pandoc.readMarkdown (def { readerExtensions = pandocExtensions }) mdContents
     filteredAst <- liftIO $ diagramsFilter ast
     Pandoc.writeNative (def { writerExtensions = pandocExtensions }) filteredAst
@@ -55,7 +60,7 @@ goldenTests = do
     [ Tasty.goldenVsString
         (takeBaseName mdFile)
         nativeFile
-        (LBS.fromStrict . encodeUtf8 <$> mdToNative mdFile)
+        (LBS.fromStrict . T.encodeUtf8 <$> mdToNative mdFile)
     | mdFile <- mdFiles
     , let nativeFile = replaceExtension mdFile ".out"
     ]
